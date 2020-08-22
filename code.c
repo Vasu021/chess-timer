7651bdddd1fa8cbe2c77d664094a5ce2b2168390
Vasu021 <amlendushekharcss_cse18@its.edu.in>
2020-08-22 23:40:11 +0530

    done

---
+++ code.c
@@ -0,0 +1,112 @@
+//code of arduino
+#include <LiquidCrystal.h>
+const int rs=9,en=8,d4=4,d5=5,d6=6,d7=7,x=4.5,trigger=3,echo=2;
+double d,t,dw;
+int tw=300,tb=300,minutes=5;
+
+double getDist();
+void displayBlack(int);
+void displayWhite(int);
+
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+void setup() {
+  lcd.begin(16,2);
+  lcd.setCursor(1,0);
+  lcd.print("Black");
+  lcd.setCursor(11,0);
+  lcd.print("White");
+  
+  lcd.setCursor(0,1);
+  lcd.print(minutes);
+  lcd.setCursor(1,1);
+  lcd.print(":");
+  lcd.setCursor(2,1);
+  lcd.print(0);
+  lcd.setCursor(3,1);
+  lcd.print(0);
+
+  lcd.setCursor(12,1);
+  lcd.print(minutes);
+  lcd.setCursor(13,1);
+  lcd.print(":");
+  lcd.setCursor(14,1);
+  lcd.print(0);
+  lcd.setCursor(15,1);
+  lcd.print(0);
+}
+
+void loop() {
+dw=getDist();
+if(dw<x){
+  while(1){
+    dw=getDist();
+    if(dw<x){
+      tb=tb-1;
+      displayBlack(tb);
+      }
+    if(dw>x){
+      tw=tw-1;
+      displayWhite(tw);
+      }
+      if(tw==0 || tb==0){
+      if(tw==0){
+        lcd.setCursor(6,1);
+        lcd.print("Black wins");
+        }
+      if(tb==0){
+        lcd.setCursor(0,1);
+        lcd.print("White wins");
+        }
+        delay(10000);
+        break;
+      }
+    delay(1000);
+    }
+    
+  }
+  
+}
+
+
+//function to get the distance
+double getDist(){
+  int intigerDistance;
+  digitalWrite(trigger,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger,LOW);
+  t=pulseIn(echo,HIGH);
+  d=0.017*t;
+  intigerDistance=(int)d;
+  return intigerDistance;
+  }
+
+
+//function to display white side
+void displayWhite(int t){
+  int m,s;
+  m=(int)t/60;
+  s=t%60;
+  
+  lcd.setCursor(12,1);
+  lcd.print(m);
+  lcd.setCursor(13,1);
+  lcd.print(":");
+  lcd.setCursor(14,1);
+  lcd.print(s);
+  }
+
+//function to display black side
+void displayBlack(int t){
+  int m,s;
+  m=(int)t/60;
+  s=t%60;
+  
+  lcd.setCursor(0,1);
+  lcd.print(m);
+  lcd.setCursor(1,1);
+  lcd.print(":");
+  lcd.setCursor(2,1);
+  lcd.print(s);
+  }
---
+++ readME.txt
@@ -0,0 +1,21 @@
+Materials used- 
+
+1. Arduino UNO
+2. Ultrasonic sensor
+3. LCD Display 16x2
+4. Bread Board
+5. Jumper Wires
+6. box with cover ( mobile phone packing box and same cover used by me )
+
+
+
+Working-
+
+When the white side is down,
+the ultra-sonic sensor senses the distance (less then x=4.5cm i.e. slightly higher then the height of box)
+and the black counter starts decreasing. When black side is  down, 
+the distance gets higher then 'x' and white timer stats decreasing.
+
+
+
+More work to do - increment in time
